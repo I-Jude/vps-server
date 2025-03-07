28036f4f10b70bef36fd5cba8bd0e978a8f9af9d
Jude_I <115415044+I-Jude@users.noreply.github.com>
2025-03-07 22:53:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+# Ensure the script runs as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root!"
+    exit 1
+fi
+
+# Install Chrome (requires superuser privileges)
 apt-get update && apt-get install -y wget unzip
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb || apt-get -fy install
+
+# Install Gunicorn
+pip install gunicorn
+
+# Start Flask with Gunicorn
 gunicorn --bind 0.0.0.0:$PORT wsgi:app
